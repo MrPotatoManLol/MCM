4dad9a200f67e730706d4af477db787ce058f28a
MrPotatoManLol <labiosanoah@gmail.com>
2023-07-30 21:04:58 -0400

    Added: - Loottables - Block Drops on mining - Ruby Ore (Deepslate & Regular)

--- src/main/java/net/mrpotatoman/mcm/MCM.java
+++ src/main/java/net/mrpotatoman/mcm/MCM.java
@@ -8,6 +8,7 @@ import net.mrpotatoman.mcm.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// TODO - Ore Generation
 public class MCM implements ModInitializer {
 	public static final String MOD_ID = "mcm";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
